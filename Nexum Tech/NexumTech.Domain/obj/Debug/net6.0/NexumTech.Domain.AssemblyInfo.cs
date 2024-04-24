209300759a6aaa91cad8c4f8e2c7ad2a7f784de3
Gabriel <gabrielab2411@gmail.com>
2024-04-24 16:53:30 -0300

    [BUGFIX] - Fixed unnecessary files

--- Nexum Tech/NexumTech.Domain/obj/Debug/net6.0/NexumTech.Domain.AssemblyInfo.cs
+++ Nexum Tech/NexumTech.Domain/obj/Debug/net6.0/NexumTech.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexumTech.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04307653ec62e4031dee3eb9de533914e14d11a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101450924ab929b5f6378016acf3a167a296fe34")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexumTech.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexumTech.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Nexum Tech/NexumTech.Infra/obj/Debug/net6.0/NexumTech.Infra.AssemblyInfo.cs
+++ Nexum Tech/NexumTech.Infra/obj/Debug/net6.0/NexumTech.Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexumTech.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04307653ec62e4031dee3eb9de533914e14d11a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101450924ab929b5f6378016acf3a167a296fe34")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexumTech.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexumTech.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
