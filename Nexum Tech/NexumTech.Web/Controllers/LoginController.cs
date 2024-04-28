6adf5340dde38371e14440b4c4d9e3a20a83aeff
Gustavo Braz <brazgustav@gmail.com>
2024-04-28 19:16:45 -0300

    [FEAT] - Added SignUp screen and modified Login screen

---
+++ Nexum Tech/NexumTech.Web/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NexumTech.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            if (email != null && password != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Usuário não encontrado ou senha incorreta";
+                return RedirectToAction("Index", "Login");
+            }
+        }
+    }
+}
