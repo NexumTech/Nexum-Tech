c43b9e42b4fe7853d162776a4cafac60d1a2e4a9
Gustavo Braz <brazgustav@gmail.com>
2024-05-29 23:09:22 -0300

    [FEAT] - Added real time chart export

--- Nexum Tech/NexumTech.Web/wwwroot/js/RealTimeChart.js
+++ Nexum Tech/NexumTech.Web/wwwroot/js/RealTimeChart.js
@@ -79,9 +79,66 @@ $(document).ready(function () {
         }
     });
 
+    $('#exportImage').on('click', function () {
+        exportChartAsImage(realTimeChart);
+    });
+
+    $('#exportPDF').on('click', function () {
+        exportChartAsPDF(realTimeChart);
+    });
+
+    $('#exportCSV').on('click', function () {
+        exportChartAsCSV(realTimeChart);
+    });
+
     $('#realTemp').on('click', function () {
         resetChart();
     });
+
+    function exportChartAsImage(chart) {
+        const a = document.createElement('a');
+        a.href = chart.toBase64Image();
+        a.download = 'NexumRealTimeChart.png';
+        a.click();
+    }
+
+    function exportChartAsPDF(chart) {
+        debugger;
+        const { jsPDF } = window.jspdf;
+        const doc = new jsPDF('landscape');
+        const imgData = chart.toBase64Image();
+
+        const pageWidth = doc.internal.pageSize.getWidth();
+        const pageHeight = doc.internal.pageSize.getHeight();
+        const imgProps = chart.canvas;
+        const imgWidth = imgProps.width;
+        const imgHeight = imgProps.height;
+        const aspectRatio = imgWidth / imgHeight;
+
+        const pdfWidth = pageWidth - 20;
+        const pdfHeight = pdfWidth / aspectRatio;
+
+        doc.addImage(imgData, 'PNG', 10, 10, pdfWidth, pdfHeight);
+
+        doc.save('NexumRealTimeChart.pdf');
+    }
+
+    function exportChartAsCSV(chart) {
+        let csvContent = 'data:text/csv;charset=utf-8,';
+        const labels = chart.data.labels.join(',');
+        csvContent += labels + '\n';
+
+        chart.data.datasets.forEach((dataset) => {
+            const dataString = dataset.data.join(',');
+            csvContent += dataset.label + ',' + dataString + '\n';
+        });
+
+        const encodedUri = encodeURI(csvContent);
+        const a = document.createElement('a');
+        a.href = encodedUri;
+        a.download = 'NexumRealTimeChart.csv';
+        a.click();
+    }
 
     function checkDifferentDates(response) {
         if (timeStamps.length >= 3) {
